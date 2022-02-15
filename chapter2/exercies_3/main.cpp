b4b6fca7539f53d1c03ba1fb241b81e3aa0018d5
forqk <educationfork@gmail.com>
2022-02-15 10:32:44 +0300

    upd

--- chapter2/exercies_3/main.cpp
+++ chapter2/exercies_3/main.cpp
@@ -8,7 +8,6 @@ int main()
     cv::Mat img1, img2;
 
     cv::namedWindow("Exercies_3", cv::WINDOW_AUTOSIZE);
-    cv::namedWindow("lol", cv::WINDOW_AUTOSIZE);
 
     cv::VideoCapture capture(0); //  0 camera index
 
@@ -18,7 +17,6 @@ int main()
         (int)capture.get( cv::CAP_PROP_FRAME_HEIGHT)/2
     );
 
-    //cv::pyrDown(img1, img2, cv::Size(img1.cols/2, img1.rows/2));
 
     cv::VideoWriter writer;
     writer.open("testwrite.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, size); // or  M P E G
@@ -28,15 +26,8 @@ int main()
     for(;;){
         capture >> original_image;
         cv::pyrDown(original_image, reduced_image);
-        cv::pyrDown(reduced_image, reduced_image2);
-        cv::pyrDown(reduced_image2, reduced_image3);
-        cv::pyrDown(reduced_image3, r_i4);
-        cv::pyrDown(r_i4, r_i5);
-        cv::pyrDown(r_i4, r_i6);
 
-        cv::imshow("Exercies_3", r_i6);
-
-        cv::imshow("lol", reduced_image2);
+        cv::imshow("Exercies_3", original_image);
 
         writer << reduced_image;
         char c = cv::waitKey(10);
--- chapter2/exercies_5/main.cpp
+++ chapter2/exercies_5/main.cpp
@@ -3,32 +3,30 @@
 
 using namespace std;
 
-int main()
-{
-    cv::Mat img1, img2;
+int main(){
+    int SliderValue  = 1;
 
-   // cv::namedWindow("Exercies_5", cv::WINDOW_AUTOSIZE);
-   // cv::namedWindow("Example2_6-out", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Exercies_5", cv::WINDOW_AUTOSIZE);
+    cv::resizeWindow("Exercies_5", 520, 380);
+
+    cv::createTrackbar("Subdiscr", "Exercies_5", &SliderValue, 8);
 
     cv::VideoCapture capture(0); //  0 camera index
-
-    double fps = capture.get(cv::CAP_PROP_FPS);
-    cv::Size size(
-        (int)capture.get( cv::CAP_PROP_FRAME_WIDTH)/2,
-        (int)capture.get( cv::CAP_PROP_FRAME_HEIGHT)/2
-    );
-
-    cv::VideoWriter writer;
-    writer.open("testwrite.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, size); // or  M P E G
 
     cv::Mat original_image, reduced_image;
 
     for(;;){
         capture >> original_image;
-        cv::pyrDown(original_image, reduced_image); //pyrDown для дискретизации применять на каждой итерации
-        cv::imshow("Exercies_3", reduced_image);
-        writer << reduced_image;
+            for(int i = 0; i != SliderValue; i++){
+                cv::pyrDown(original_image, reduced_image);
+
+                original_image = reduced_image;
+            }
+
+        cv::imshow("Exercies_5", reduced_image);
+
         char c = cv::waitKey(10);
+
         if(c == 27) break;
     }
 
